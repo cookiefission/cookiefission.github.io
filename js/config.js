d3a4ef3795a331d241d92f5032294e84a756da1b
Sean Kenny <sean.kenny@hotmail.com>
2014-06-05 21:30:26 +0100

    Added grunt-bower-requirejs

---
+++ js/config.js
@@ -0,0 +1,13 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    bpopup: "../bower_components/bpopup/jquery.bpopup",
+    jquery: "../bower_components/jquery/jquery",
+    requirejs: "../bower_components/requirejs/require"
+  },
+  packages: [
+
+  ]
+});
